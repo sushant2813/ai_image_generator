7f7fd2597d724957592850413f0652d1634a9a84
sushant2813 <153453603+sushant2813@users.noreply.github.com>
2025-02-07 19:59:14 +0530

    apikeyupdate

--- server/controllers/GenerateImage.js
+++ server/controllers/GenerateImage.js
@@ -4,7 +4,6 @@ import OpenAI from "openai";
 
 dotenv.config();
 
-// Setup OpenAI API
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -18,14 +17,19 @@ export const generateImage = async (req, res, next) => {
     }
 
     const response = await openai.images.generate({
-      prompt,
+      model: "dall-e-3",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
-      response_format: "b64_json",
+      response_format: "b64_json", // Requesting Base64 format
     });
 
+    if (!response.data || response.data.length === 0) {
+      return next(createError(500, "Failed to generate image"));
+    }
+
     const generatedImage = response.data[0].b64_json;
-    res.status(200).json({ photo: generatedImage });
+    res.status(200).json({ photo: generatedImage }); // Returning Base64 data
   } catch (error) {
     next(
       createError(
